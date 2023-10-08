a55c7d3b597265571381851cb1104fbb60692eb0
Caneishappy <87881430+Caneishappy@users.noreply.github.com>
2023-10-08 23:09:48 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,196 +1,269 @@
-const grid = [
-    [
-        document.querySelector("#item0"),
-        document.querySelector("#item1"),
-        document.querySelector("#item2"),
-    ],
-    [
-        document.querySelector("#item3"),
-        document.querySelector("#item4"),
-        document.querySelector("#item5"),
-    ],
-    [
-        document.querySelector("#item6"),
-        document.querySelector("#item7"),
-        document.querySelector("#item8"),
-    ],
-];
-let state = [
-    ["", "", ""],
-    ["", "", ""],
-    ["", "", ""],
-];
+const elFake = document.createElement("div");
+
+const elGameOver = document.getElementById("Gameover-screen") ?? elFake;
+const elBG = document.getElementById("grid-container") ?? elFake;
+let getSquare = undefined;
+
 let circle = false;
 let gameover = false;
-for (let i = 0; i < grid.length; i++) {
-    for (let j = 0; j < grid[i].length; j++) {
-        let meinDiv = grid[i][j];
-        meinDiv.addEventListener("click", function () {
-            if (meinDiv.innerHTML == `` && !gameover) {
-                if (!circle) {
-                    meinDiv.innerHTML = `X`;
-                    state[i][j] = `X`;
-                } else {
-                    meinDiv.innerHTML = `O `;
-                    state[i][j] = `O`;
-                }
-                circle = !circle;
-                if (resultnoai(grid)) {
-                    console.log("Game over!");
-                    gameover = true;
-                    const Gameoverscreen =
-                        document.getElementById("Gameover-screen");
-                    Gameoverscreen.style.display = "block";
-                    const bg = document.getElementById("grid-container");
-                    bg.style.background = "red";
-                } else if (checkTienoai(grid)) {
-                    console.log("Game over! Draw");
-                    const Gameoverscreen =
-                        document.getElementById("Gameover-screen");
-                    Gameoverscreen.style.display = "block";
-                    const bg = document.getElementById("grid-container");
-                    bg.style.background = "#ff6500";
-                }
+let aiEnabled = true;
+let boardsize = 3;
+
+const grid = [];
+let state = [
+    // ["", "", ""],
+    // ["", "", ""],
+    // ["", "", ""],
+];
+
+function enableAi() {
+    aiEnabled ? console.log("Ai is not off") : console.log("Ai is now on.");
+    aiEnabled = !aiEnabled;
+}
+
+function initGrid(size) {
+    boardsize = size;
+    //Delete content of the board, grid & state
+    while (elBG.firstChild) {
+        elBG.removeChild(elBG.firstChild);
+    }
+    state.length = 0;
+    for (let i = grid.length - 1; i >= 0; i--) {
+        for (let u = grid[0].length - 1; u >= 0; u--) {
+            grid[i].splice(u, 1);
+        }
+        grid.splice(i, 1);
+    }
+
+    //Display the div elements in a quare
+    csssize = "'";
+    for (let n = 1; n < size + 1; n++) {
+        for (let b = 0; b < size; b++) {
+            csssize = csssize + " " + n.toString();
+        }
+        if (n < size) csssize = csssize + "' '";
+    }
+    elBG.style["grid-template-areas"] = csssize;
+
+    //Add all elements to the Grid and state
+    for (let i = 0; i < size; i++) {
+        grid.push([]);
+        state.push([]);
+        for (let j = 0; j < size; j++) {
+            const [row, col] = [i, j];
+            state[row].push("");
+            if (!grid[row][col]) {
+                //crate the div element with the right id & add it the the grid
+                const newItem = document.createElement("div");
+                newItem.setAttribute("id", `${"item" + [row * size + col]}`);
+                let newSquare = elBG.appendChild(newItem);
+                grid[row].push(newSquare);
             }
-        });
+            grid[row][col].addEventListener("click", function () {
+                //add the logic to the square
+                getSquare = `${"item" + [row * size + col]}`;
+                if (
+                    document.getElementById(getSquare).textContent !== "" ||
+                    gameover
+                ) {
+                    return;
+                }
+                //set the selected square and if on let the alg. make a move
+                setSquare(row, col);
+                if (!gameover && aiEnabled) {
+                    setSquare(...aiMove(state));
+                }
+            });
+        }
     }
 }
 
-function resetgrid() {
-    for (let i = 0; i < grid.length; i++) {
-        for (let j = 0; j < grid[i].length; j++) {
-            const meinDiv = grid[i][j]; //Mein div entfernen
-            meinDiv.innerHTML = ``;
-        }
-    }
-    resetState();
-    gameover = false;
-    circle = false;
-    console.log(`Reset`);
-    const Gameoverscreen = document.getElementById("Gameover-screen");
-    Gameoverscreen.style.display = "none";
-    const bg = document.getElementById("grid-container");
-    bg.style.background = "#2196F3";
-}
-function checkTienoai(grid) {
-    for (const i in grid) {
-        for (const j in grid) {
-            if (grid[i][j].innerHTML == "") {
-                return false;
-            }
-        }
-    }
-    return true;
-}
-
-function resultnoai(grid) {
-    //horizontal
-    for (let i = 0; i < grid.length; i++) {
-        if (
-            grid[i][0].innerHTML === grid[i][1].innerHTML &&
-            grid[i][1].innerHTML === grid[i][2].innerHTML &&
-            grid[i][0].innerHTML !== ""
-        ) {
-            return true;
-            // console.log("GAME OVER! : Horizontal");
-        }
-        //vertical
-        // for (let i = 0; i < grid.length; i++) {
-        else if (
-            grid[0][i].innerHTML === grid[1][i].innerHTML &&
-            grid[1][i].innerHTML === grid[2][i].innerHTML &&
-            grid[0][i].innerHTML !== ""
-        ) {
-            return true;
-        }
-    }
-    //diagonal
-    if (
-        (grid[0][0].innerHTML === grid[1][1].innerHTML &&
-            grid[0][0].innerHTML === grid[2][2].innerHTML &&
-            grid[0][0].innerHTML !== "") ||
-        (grid[0][2].innerHTML === grid[1][1].innerHTML &&
-            grid[1][1].innerHTML === grid[2][0].innerHTML &&
-            grid[1][1].innerHTML !== "")
-    ) {
-        // console.log("Game Over: Diagunal");
-        return true;
-    }
-    return false;
-}
-
-//For the Algorithen to calculate the best Move:
-function resetState() {
-    for (i in grid) {
-        state[i] = []; // Initialize the inner array
-        for (j in grid[i]) {
+function resetGrid() {
+    console.log(aiEnabled);
+    for (let i = 0; i < boardsize; i++) {
+        for (let j = 0; j < boardsize; j++) {
+            grid[i][j].innerHTML = "";
             state[i][j] = "";
         }
     }
+    circle = false;
+    gameover = false;
+
+    console.log("Reset");
+    elGameOver.style.display = "none";
+    elBG.style.background = "#2196F3";
 }
-function checkTie(state) {
-    for (i in state) {
-        for (j in state) {
-            if (state[i][j] == "") {
-                return false;
+
+function setSquare(row, col) {
+    const playerSymbol = circle ? "O" : "X";
+    circle = !circle;
+
+    grid[row][col].innerHTML = playerSymbol;
+    state[row][col] = playerSymbol;
+    checkGameOver();
+}
+
+function checkGameOver() {
+    const condition = getCondition(state);
+
+    if (condition !== undefined) {
+        gameover = true;
+        if (condition === 0) {
+            console.log("Game over! Draw");
+            elGameOver.style.display = "block";
+            elBG.style.background = "#ff6500";
+        } else {
+            console.log("Game over!");
+            elGameOver.style.display = "block";
+            elBG.style.background = "red";
+        }
+    }
+}
+
+function aiMove(state) {
+    const [value, action] = minimax(state);
+    return action;
+}
+
+function minimax(state) {
+    //recursive
+    const condition = getCondition(state);
+    if (condition !== undefined) {
+        // condition == undefined would mean that the game is ongoing.
+        return [condition, undefined];
+    } else {
+        if (getPlayer(state) === "X") {
+            let value = -2; //So every situation is better
+            let valueAction = undefined;
+            for (const action of getAvailableActions(state)) {
+                //For every possible move:
+                const [newValue] = minimax(getNewState(state, action));
+                if (newValue > value) {
+                    //Getting the best value. -1 Means O won; 0 mean draw; 1 means X won.
+                    value = newValue;
+                    valueAction = action;
+                }
+            }
+            return [value, valueAction];
+        } else {
+            let value = 2;
+            let valueAction = undefined;
+            for (const action of getAvailableActions(state)) {
+                const [newValue] = minimax(getNewState(state, action));
+                if (newValue < value) {
+                    value = newValue;
+                    valueAction = action;
+                }
+            }
+            return [value, valueAction];
+        }
+    }
+}
+
+let LengthToWin = boardsize; //might change later on, so it is a own variable
+
+function getCondition(state) {
+    for (let i = 0; i <= 1; i++) {
+        // Twice for switching row and col
+        let highestcount = 0;
+        for (let k in state) {
+            let oldsymbole = "";
+            let count = 0;
+            const [row, col] = i === 0 ? [0, k] : [k, 0]; // Switch the label of row and col for horizontal/vertical win.
+
+            for (let j in state[row]) {
+                const [currRow, currCol] = i === 0 ? [j, k] : [k, j]; // Switch the label of row and col for horizontal/vertical win.
+                const currentSymbol = state[currRow][currCol];
+
+                if (currentSymbol !== "") {
+                    if (currentSymbol === oldsymbole) {
+                        count++;
+                    } else {
+                        count = 1; // Start counting from 1 for the current symbol.
+                        oldsymbole = currentSymbol;
+                    }
+
+                    highestcount = Math.max(count, highestcount);
+
+                    if (highestcount >= LengthToWin) {
+                        return oldsymbole === "X" ? 1 : -1;
+                    }
+                } else {
+                    count = 0;
+                    oldsymbole = "";
+                }
+            }
+
+            //Diagonal
+            count = 0; //Reseting the count to 0
+            if (row >= LengthToWin - 1 || col >= LengthToWin - 1) {
+                i === 0 ? (j = Math.max(row, col)) : (j = 0);
+
+                for (let n = 0; n <= Math.max(row, col); n++) {
+                    //For the length of the Diagonal (the diagonal has the same lenth as the row/col that it starts/stops on).
+                    const [currRow, currCol] = i === 0 ? [j, n] : [n, j];
+
+                    const currentSymbol = state[currRow][currCol];
+
+                    if (currentSymbol !== "") {
+                        if (currentSymbol === oldsymbole) {
+                            count++;
+                        } else {
+                            count = 1; // Start counting from 1 for the current symbol
+                            oldsymbole = currentSymbol;
+                        }
+
+                        highestcount = Math.max(count, highestcount);
+
+                        if (highestcount >= LengthToWin) {
+                            return oldsymbole === "X" ? 1 : -1;
+                        }
+                    } else {
+                        count = 0;
+                        oldsymbole = "";
+                    }
+                    i === 0 ? j-- : j++;
+                }
             }
         }
     }
-    return true;
-}
-function result(grid) {
-    for (let i = 0; i < grid.length; i++) {
-        if (
-            grid[i][0] === grid[i][1] &&
-            grid[i][1] === grid[i][2] &&
-            grid[i][0] !== ""
-        ) {
-            return {
-                Value: true,
-                Winner: grid[i][0],
-            };
-        } else if (
-            grid[0][i] === grid[1][i] &&
-            grid[1][i] === grid[2][i] &&
-            grid[0][i] !== ""
-        ) {
-            return {
-                Value: true,
-                Winner: grid[0][i],
-            };
+
+    //If no win is found, check if it is a draw or the game is ongoing.
+    for (const i in state) {
+        for (const j in state) {
+            if (state[i][j] === "") {
+                return undefined;
+            }
         }
-    } //diagonal
-    if (
-        grid[0][0] === grid[1][1] &&
-        grid[0][0] === grid[2][2] &&
-        grid[0][0] !== ""
-    ) {
-        return {
-            Value: true,
-            Winner: grid[0][0],
-        };
     }
-    if (
-        grid[0][2] === grid[1][1] &&
-        grid[1][1] === grid[2][0] &&
-        grid[1][1] !== ""
-    ) {
-        return {
-            Value: true,
-            Winner: grid[0][2],
-        };
-    }
-    return {
-        Value: false,
-        Winner: undefined,
-    };
+
+    return 0; //Draw
 }
 
-function getActions(state) {
+function getPlayer(state) {
+    let actions = 0;
+    //If the available amount of squars is even, then 'O' has to make a move.
+    for (const i in state) {
+        for (const j in state[i]) {
+            if (state[i][j] !== "") {
+                actions++;
+            }
+        }
+    }
+
+    function isEven(x) {
+        return x % 2 === 0;
+    }
+
+    return isEven(actions) ? "X" : "O";
+}
+
+function getAvailableActions(state) {
     const actions = [];
     for (const i in state) {
         for (const j in state[i]) {
-            if (state[i][j] == "") {
+            if (state[i][j] === "") {
+                //Add every empty square to the list.
                 actions.push([i, j]);
             }
         }
@@ -198,107 +271,16 @@ function getActions(state) {
     return actions;
 }
 
-function getNewState(state, action, tomove) {
-    const newState = cloneState(state);
-    newState[action[0]][action[1]] = tomove;
+function getNewState(state, action) {
+    //Getting the state(/"playing feld") if the move 'action' is made.
+    const newState = state.map((row) => [...row]);
+    const [i, j] = action;
+    newState[i][j] = getPlayer(state);
     return newState;
 }
-
-function cloneState(state) {
-    const newState = [];
-    for (const i in state) {
-        newState.push([]);
-        for (const j in state[i]) {
-            newState[i].push(state[i][j]);
-        }
-    }
-    return newState;
+function doAiMove(state) {
+    console.time();
+    setSquare(...aiMove(state));
+    console.timeEnd();
 }
-
-function terminal(state) {
-    // Is the Game over? And if so how Won or is it a Tie. (returns -1 0 or 1)
-    let isgameover = checkTie(state).Value;
-    let isWinner = result(state).Winner;
-    if (isgameover) {
-        return 0; // Tie
-    }
-    if (isWinner == "X") {
-        return 1; // Max won
-    }
-    return -1; //Min won
-}
-
-let moves = 0;
-function player(state) {
-    // Who is to make a move?
-    moves = 0;
-    for (const i in state) {
-        // How many squares are taken?
-        for (const j in state[i]) {
-            if (state[i][j] != "") {
-                moves++;
-            }
-        }
-    }
-    // if (moves == 9) {
-    //     return "max";
-    // }
-    if (moves % 2 == 0) {
-        // If the amout of sqares taken is even, that Max is to move, because Max is always first to move.
-        return "max";
-    }
-    return "min"; // Else Min is to move.
-}
-
-let action = null;
-let newval = 0;
-let comparval = 0;
-
-function minimax(s, move) {
-    // Check if game is over and who won.
-    if (checkTie(s) || result(s).Value) {
-        // value = terminal(state);
-        return {
-            Action: move,
-            Value: terminal(state), // idk if terminal(state); works or if it would brake sth.
-        };
-    }
-    if (player(s) == "max") {
-        newval = -Infinity; // so if first Value that is find is greater.
-        for (a of getActions(s)) {
-            comparval = minimax(getNewState(s, a, "X"), a).Value;
-            if (newval < comparval) {
-                // compare the new value to the last.
-                newval = comparval;
-                action = a;
-                if (newval === 1) {
-                    // If a winning move is found don't serch further.
-                    break;
-                }
-            }
-        }
-        return {
-            Action: action,
-            Value: newval,
-        };
-    }
-    if (player(s) == "min") { // min is the O player and wants to minimize the value
-        newval = Infinity; // so if first Value that is found is lower.
-        for (a of getActions(s)) {
-            //for every possibe move
-            comparval = minimax(getNewState(s, a, "O"), a).Value;
-            if (newval > comparval) {
-                newval = comparval;
-                action = a;
-                if (newval === -1) {
-                    // If a winning move is found don't serch further.
-                    break;
-                }
-            }
-        }
-        return {
-            Action: action,
-            Value: newval,
-        };
-    }
-}
+initGrid(boardsize);
